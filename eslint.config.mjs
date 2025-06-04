a10e9ae96e6dd84a48792f434a250f0793eb4873
MD. MINHAZ <mdm047767@gmail.com>
2025-06-04 20:30:39 +0000

    Add ESLint configuration for code quality and linting

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      indent: "off",
+    },
+  },
+  prettier,
+]);
